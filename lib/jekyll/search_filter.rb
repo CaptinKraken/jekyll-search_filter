abf7de2b52dfe9632e499f0e3b7f3e320f32f993
Greg Scott <i@gregoryjscott.com>
2015-07-16 12:46:59 -0500

    Fix to support collections

--- lib/jekyll/search_filter.rb
+++ lib/jekyll/search_filter.rb
@@ -6,12 +6,25 @@ module Jekyll
     def search(input, property, *args)
       return [] if input.nil?
       input.select do |item|
-        property_value = item[property.to_s]
+        property_value = item_property(item, property)
         if property_value.is_a?(Enumerable)
           args.any? { |arg| property_value.include?(arg) }
         else
           args.any? { |arg| arg == property_value }
         end
+      end
+    end
+
+    private
+
+    # Copied from https://github.com/jekyll/jekyll/blob/92a9582733dfee0f359e491a8fae236e91087a54/lib/jekyll/filters.rb#L314-L322
+    def item_property(item, property)
+      if item.respond_to?(:to_liquid)
+        item.to_liquid[property.to_s]
+      elsif item.respond_to?(:data)
+        item.data[property.to_s]
+      else
+        item[property.to_s]
       end
     end
 
